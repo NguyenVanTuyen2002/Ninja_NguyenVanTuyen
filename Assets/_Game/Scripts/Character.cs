8d4364fdf7ef05a437c10d4da54a672fa1aa1401
NguyenVanTuyen2002 <nguyenvantuyentsdemon@gmail.com>
2024-04-25 17:38:14 +0700

    +Push_Ninja
    Begin

---
+++ Assets/_Game/Scripts/Character.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] protected HealthBar healthBar;
+
+    private float hp;
+    string currentAnimName;
+
+    public bool IsDead => hp <= 0;
+
+    private void Start()
+    {
+        OnInit();
+    }
+
+    public virtual void OnInit()
+    {
+        Debug.Log("On Init");
+        hp = 100;
+        healthBar.OnInit(100, transform);
+    }
+    public virtual void OnDespawn()
+    {
+
+    }
+
+    protected virtual void OnDeath()
+    {
+        ChangeAnim("die");
+        Invoke(nameof(OnDespawn), 2f);
+    }
+
+    protected void ChangeAnim(string animName)
+    {
+        if (currentAnimName != animName)
+        {
+            anim.ResetTrigger(animName);
+            currentAnimName = animName;
+            anim.SetTrigger(currentAnimName);
+        }
+    }
+
+    public void OnHit(float damage)
+    {
+        if (!IsDead)
+        {
+            hp -= damage;
+
+            if (IsDead)
+            {
+                hp = 0;
+                OnDeath();
+            }
+            healthBar.SetNewHp(hp);
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Player.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float jumpForce = 350;
+    [SerializeField] bool isGrounded = true;
+    [SerializeField] private Kunai kunaiPrefab;
+    [SerializeField] private Transform throwPoint;
+    [SerializeField] private GameObject attackArea;
+
+    bool isJumping = false;
+    bool isAttack = false;
+    private bool isDeath = false;
+    float horizontal;
+    private int coin = 0;
+    private Vector3 savePoint;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (IsDead)
+        {
+            return;
+        }
+
+
+        //Debug.Log(CheckGround());
+        isGrounded = CheckGround();
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (isAttack)
+        {
+            return;
+        }
+
+        if(isGrounded)
+        {
+            if (isJumping)
+            {
+                return;
+            }
+
+            //jump
+            if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+            {
+                Jump();
+            }
+
+            //change anim run
+            if (Mathf.Abs(horizontal) > 0.1f)
+            {
+                ChangeAnim("run");
+            }
+
+            //attack
+            if (Input.GetKeyDown(KeyCode.C) && isGrounded)
+            {
+                Attack();
+            }
+
+            //throw
+            if (Input.GetKeyDown(KeyCode.V) && isGrounded)
+            {
+                Throw();
+            }
+        }
+
+        //check falling
+        if (!isGrounded && rb.velocity.y < 0)
+        {
+            //Debug.Log("Falling");
+            ChangeAnim("fall");
+            isJumping = false;
+        }
+
+        //Moving
+        if (Mathf.Abs(horizontal) > 0.1f)
+        {
+            ChangeAnim("run");
+            rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
+            transform.rotation = Quaternion.Euler(new Vector3(0, horizontal > 0 ? 0 : 180, 0));
+        }
+        else if(isGrounded)        
+        {
+            //Debug.Log("Idle");
+            ChangeAnim ("idle");
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    public override void OnInit()
+    {
+        base.OnInit();
+        isAttack = false;
+        transform.position = savePoint;
+        ChangeAnim("idle");
+        DeActiceAttack();
+        SavePoint();
+    }
+
+    public override void OnDespawn()
+    {
+        base.OnDespawn();
+        OnInit();
+    }
+
+    protected override void OnDeath()
+    {
+        base.OnDeath();
+    }
+    private bool CheckGround()
+    {
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * 1.1f, Color.red);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayer);
+        
+        /*if (hit.collider != null )
+        {
+            return true;
+        }
+        else
+        { 
+            return false;
+        }*/
+
+        return hit.collider != null;
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack");
+        isAttack = true;
+        ChangeAnim("attack");
+        Invoke(nameof(ResetAttack), 0.5f);
+        ActiceAttack();
+        Invoke(nameof(DeActiceAttack), 0.5f);
+    }
+
+    private void Throw()
+    {
+        ChangeAnim("throw");
+        isAttack = true;
+        Invoke(nameof(ResetAttack), 0.5f);
+
+        Instantiate(kunaiPrefab, throwPoint.position, throwPoint.rotation);
+    }
+
+    private void ResetAttack()
+    {
+        Debug.Log("reset");
+        isAttack = false;
+        ChangeAnim("idle");
+    }
+
+    private void Jump()
+    {
+        //Debug.Log("Jump");
+        isJumping = true;
+        ChangeAnim("jump");
+        rb.AddForce(jumpForce * Vector2.up);
+    }
+
+    internal void SavePoint()
+    {
+        savePoint = transform.position;
+    }
+    private void ActiceAttack()
+    {
+        attackArea.SetActive(true);
+    }
+
+    private void DeActiceAttack()
+    {
+        attackArea.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coin"))
+        {
+            Debug.Log("ăn coin ");
+            coin++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.tag == "Deathzone")
+        {
+            ChangeAnim("die");
+            Invoke(nameof(OnInit), 1f);
+        }
+    }
+}
+// ko dịch chuyển về vị trí đã lưu
